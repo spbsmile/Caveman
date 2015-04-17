353e56503ef084d74432b852bf1460fd3c6ebe80
Makar Zabiyakin <zabiyakin.makar@gmail.com>
2015-04-17 19:59:33 +0300

    немного TileMap

---
+++ Assets/TileMap.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class TileMap : MonoBehaviour
+{
+    public Transform container;
+    public int width = 15;
+    public int height = 15;
+
+    private float tileSize = 1.5f;
+
+    private const string TileName = "Map/ground1";
+
+	public void Start ()
+	{
+	    for (var i = 0; i < width; i++)
+	    {
+            for (var j = 0; j < height; j++)
+	        {
+                var gameObject = Instantiate(Resources.Load(TileName, typeof(GameObject)) as GameObject);
+                gameObject.transform.SetParent(container);
+                gameObject.transform.position = new Vector2(i * tileSize, j * tileSize);
+
+	        }  
+	    }
+        
+
+
+
+
+	}
+	
+	public void Update () 
+    {
+	
+	}
+}
