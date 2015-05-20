30a74bbb93374acfb11018ed860d1e26a38c48a3
Zabiyakin Makar <zabiyakin.makar@gmail.com>
2015-05-20 19:50:18 +0300

    класс ObjectPool

---
+++ Assets/Source/ObjectPool.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Source
+{
+    public class ObjectPool<T> where T : class ,new ()
+    {
+        private Stack<T> objectStack;
+
+        private Action<T> resetAction;
+        private Action<T> onetimeInitAction;
+
+        public ObjectPool(int initialBufferSize, Action<T> ResetAction = null, Action<T> OnetimeInitAction = null) 
+        {
+            objectStack = new Stack<T>(initialBufferSize);
+            resetAction = ResetAction;
+            onetimeInitAction = OnetimeInitAction;
+        }
+
+        public T New()
+        {
+            if (objectStack.Count > 0)
+            {
+                T t = objectStack.Pop();
+                if (resetAction != null)
+                {
+                    resetAction(t);
+                }
+                return t;
+            }
+            else
+            {
+                var t = new T();
+                if (onetimeInitAction != null)
+                {
+                    onetimeInitAction(t);
+                }
+                return t;
+            }
+        }
+
+        public void Store(T obj)
+        {
+             objectStack.Push(obj);            
+        }
+    }
+}
