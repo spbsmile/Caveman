30343de43de1d28b9c56e9a2173070a826fde3b5
Zabiyakin Makar <zabiyakin.makar@gmail.com>
2015-06-18 01:24:40 +0300

    первая рабочая версия джостика

--- Assets/Source/Players/PlayerModel.cs
+++ Assets/Source/Players/PlayerModel.cs
@@ -1,24 +1,10 @@
-﻿using System.Collections;
-using Caveman.Setting;
-using Caveman.Utils;
+﻿using Caveman.Setting;
 using UnityEngine;
 
 namespace Caveman.Players
 {
     public class PlayerModel : BasePlayerModel
     {
-        const float RotateSpeed = 15f;
-       // private CNAbstractController movementJoystick;
-        private Transform _transformCache;
-
-        protected override void Start()
-        {
-            base.Start();
-            //todo realy &
-            _transformCache = GetComponent<Transform>();
-            
-        }
-
         public void Update()
         {
             ThrowStoneOnTimer();
@@ -39,29 +25,15 @@ namespace Caveman.Players
 
         public void SetJoystick(CNAbstractController movementJoystick)
         {
-          //  this.movementJoystick = movementJoystick;
             movementJoystick.ControllerMovedEvent += MovePlayer;
         }
 
         private void MovePlayer(Vector3 movement, CNAbstractController arg2)
         {
-            StopCoroutine("RotateCoroutine");
-            StartCoroutine("RotateCoroutine", movement);
-
-            _transformCache.position += _transformCache.up * movement.magnitude * Settings.SpeedPlayer * Time.deltaTime;
-            //var position = new Vector3(transform.position.x + movement.x * Time.deltaTime,
-            //   transform.position.y + movement.y * Time.deltaTime);
-            //transform.position = position;
-        }
-
-        IEnumerator RotateCoroutine(Vector3 direction)
-        {
-            do
-            {
-                _transformCache.up = Vector3.Lerp(_transformCache.up, direction, RotateSpeed * Time.deltaTime);
-                yield return null;
-            }
-            while ((direction - _transformCache.up).sqrMagnitude > 0.2f);
+            var position = new Vector3(transform.position.x + movement.x * Time.deltaTime * Settings.SpeedPlayer,
+               transform.position.y + movement.y * Time.deltaTime * Settings.SpeedPlayer);
+            transform.position = position;
+            animator.SetFloat(movement.y > 0 ? Settings.AnimRunB : Settings.AnimRunF, Settings.SpeedPlayer);
         }
     }
 }
