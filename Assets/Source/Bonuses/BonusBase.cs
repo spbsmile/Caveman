722027081c322c0aa142e14cad3dba4f0eb6e0aa
Makar <zabiyakin.makar@gmail.com>
2015-06-26 22:40:26 +0300

    cleanup

--- Assets/Source/Bonuses/BonusBase.cs
+++ Assets/Source/Bonuses/BonusBase.cs
@@ -1,53 +1,48 @@
-﻿using UnityEngine;
-using Caveman.Utils;
-using Caveman.Players;
+﻿using Caveman.Players;
 using Caveman.Setting;
+using Caveman.Utils;
+using UnityEngine;
+using Random = System.Random;
 
 namespace Caveman.Bonuses
 {
-	public class BaseBonus : MonoBehaviour
-	{
-		protected Animator animator;
-		
-		private ObjectPool pool;
-		private string name;
-		private float duration;
-		private System.Random r; 
-		
-		public void Start()
-		{
-			animator = GetComponent<Animator>();
-		}
-		
-		public void Init(System.Random random, ObjectPool pool, string name)
-		{
-			this.name = name;
-			this.pool = pool;
-			r = random;
-			
-		}
+    public class BonusBase : MonoBehaviour
+    {
+        protected Animator animator;
 
-		public void RandomPosition()
-		{
-			transform.position = new Vector2(r.Next(-Settings.Br, Settings.Br), r.Next(-Settings.Br, Settings.Br));
-		}
-		
-		public void OnTriggerEnter2D(Collider2D other)
-		{
-			if(other.gameObject.GetComponent<BasePlayerModel>())
-			{
-				
-			}	            
-		}  
-		
-		public virtual void Effect(BasePlayerModel playerModel)
-		{
-			
+        private float duration;
+        private ObjectPool pool;
+        private Random r;
+
+        public void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        public void Init(Random random, ObjectPool pool, string name)
+        {
+            this.name = name;
+            this.pool = pool;
+            r = random;
+        }
+
+        public void RandomPosition()
+        {
+            transform.position = new Vector2(r.Next(-Settings.Br, Settings.Br), r.Next(-Settings.Br, Settings.Br));
+        }
+
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.GetComponent<PlayerModelBase>())
+            {
+            }
+        }
+
+        public virtual void Effect(PlayerModelBase playerModel)
+        {
             //playerModel.Speed = playerModel.Speed*2;
             ///// no func
             //playerModel.ResetBonus -= playerModel.Speed = playerModel.Speed/2;
-		}
-	}
+        }
+    }
 }
-
-
