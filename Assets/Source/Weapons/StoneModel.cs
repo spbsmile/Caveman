24d91773a91fb6b663b8c1486058e453ff44690d
Dima <dmitriy.kirakosyan@gmail.com>
2015-07-12 16:10:50 +0300

    stones dissapearing fix

--- Assets/Source/Weapons/StoneModel.cs
+++ Assets/Source/Weapons/StoneModel.cs
@@ -24,6 +24,7 @@ namespace Caveman.Weapons
 
         public override void Destroy()
         {
+            bezierTime = 0;
             CreateStoneFragments(transform.position);
             base.Destroy();
         }
