999173603cee20bbba4981075ece38c0b66bb819
Makar <zabiyakin.makar@gmail.com>
2015-07-24 20:30:25 +0300

    const map when multiplayer

--- Assets/Source/Level/TileMap.cs
+++ Assets/Source/Level/TileMap.cs
@@ -13,12 +13,13 @@ namespace Caveman.Level
         public Transform prefabDot;
         public Transform prefabPool;
         public Transform prefabGround;
+        public bool multiplayer;
 
         public void Start()
         {
             var r = new Random();
             var tileSize = Instantiate(prefabGround).GetComponent<SpriteRenderer>().bounds.size;
-            Settings.HeightMap = heightMap - (int)tileSize.x;
+            Settings.HeightMap = heightMap - (int) tileSize.x;
             var tilesOnWidth = widthMap/tileSize.x;
             var tilesOnHeight = heightMap/tileSize.y;
 
@@ -32,20 +33,53 @@ namespace Caveman.Level
                 }
             }
 
+            var deltaPoolBounds = (float)widthMap / (dotCount - dotCount / 3);
             for (var i = 0; i < dotCount; i++)
             {
                 var gameObject = Instantiate(prefabDot);
                 gameObject.transform.SetParent(transform);
-                gameObject.transform.position = new Vector2(r.Next(widthMap),
-                    r.Next(heightMap));
+                if (!multiplayer)
+                {
+                    gameObject.transform.position = new Vector2(r.Next(widthMap),
+                        r.Next(heightMap));
+                }
+                else
+                {
+                    if (i < dotCount/3)
+                    {
+                        gameObject.transform.position = new Vector2(((float)widthMap/2 - i * deltaPoolBounds), i * deltaPoolBounds);                                
+                    }
+                    else
+                    {
+                        gameObject.transform.position = new Vector2((widthMap - (i - dotCount / 3 )* deltaPoolBounds), i * deltaPoolBounds);    
+                    }
+                }
             }
 
             for (var i = 0; i < poolCount; i++)
             {
                 var gameObject = Instantiate(prefabPool);
                 gameObject.transform.SetParent(transform);
-                gameObject.transform.position = new Vector2(r.Next(widthMap),
-                    r.Next(heightMap));
+                if (!multiplayer)
+                {
+                    gameObject.transform.position = new Vector2(r.Next(widthMap),
+                    r.Next(heightMap));    
+                }
+                else
+                {
+                    gameObject.transform.position = new Vector2((widthMap / (poolCount*2)) * (i + 2), (heightMap / (poolCount*2)) * (i + 2));
+                }
+            }
+
+            if (multiplayer)
+            {
+                var delta = (float)widthMap / (dotCount - dotCount / 1.5f);
+                for (var i = 0; i < (dotCount - dotCount / 1.5f); i++)
+                {
+                    var gameObject = Instantiate(prefabDot);
+                    gameObject.transform.SetParent(transform);
+                    gameObject.transform.position = new Vector2(widthMap - (i *delta), i*delta);
+                }
             }
         }
     }
