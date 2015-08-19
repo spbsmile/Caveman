7fd3e61e26129708d1723c37ef5f091f5398ee97
Makar <zabiyakin.makar@gmail.com>
2015-08-20 00:54:25 +0300

    improve ResultRound

--- Assets/Source/GUI/Windows/ResultRound.cs
+++ Assets/Source/GUI/Windows/ResultRound.cs
@@ -2,13 +2,16 @@
 using Caveman.Utils;
 using UnityEngine;
 using System.Collections;
+using System.Linq;
 using UnityEngine.UI;
 
 namespace Caveman.UI.Windows
 {
     public class ResultRound : MonoBehaviour
     {
-        public Transform prefabText;
+        public Transform names;
+        public Transform kills;
+        public Transform deaths;
 
         private PlayerPool poolPlayers;
 
@@ -17,7 +20,6 @@ namespace Caveman.UI.Windows
             StartCoroutine(DisplayResult());
         }
 
-        //Todo gui не должен ,наверно, пор пул игроков. ему должно передваться все , что надо . но не пул
         public void SetPlayerPool(PlayerPool pool)
         {
             poolPlayers = pool;
@@ -27,28 +29,20 @@ namespace Caveman.UI.Windows
         {
             yield return new WaitForSeconds(1f);
             Time.timeScale = 0.00001f;
-
-            var namePlayer = transform.GetChild(1);
-            var kills = transform.GetChild(2);
-            var deaths = transform.GetChild(3);
-            var axisY = -20;
-            const int deltaY = 30;
-            for (var i = 0; i < Settings.BotsCount + 1; i++)
-            {
-                //Write(poolPlayers.GetName(i), namePlayer, axisY);
-                //Write(poolPlayers.GetKills(i), kills, axisY);
-                //Write(poolPlayers.GetDeaths(i), deaths, axisY);
-                axisY -= deltaY;
-            }
+            var players = poolPlayers.GetCurrentPlayers();
+            //for (var i = 0; i < players.Count(); i++)
+            //{
+            //    players[i].
+            //}
+          
         }
 
-        private void Write(string value, Transform parent, int axisY)
-        {
-            var goName = Instantiate(prefabText);
-            goName.transform.SetParent(parent);
-            goName.transform.localPosition = new Vector2(-2, axisY);
-            goName.transform.rotation = Quaternion.identity;
-            goName.GetComponent<Text>().text = value;
-        }
+        //private void Write(string value, Transform parent)
+        //{
+        //    goName.transform.SetParent(parent);
+        //    goName.transform.localPosition = new Vector2(-2, axisY);
+        //    goName.transform.rotation = Quaternion.identity;
+        //    goName.GetComponent<Text>().text = value;
+        //}
     }
 }
