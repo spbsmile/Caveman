90a7d0a1d9b6ecdcc446e7ad3903411d3f0eb72f
VegK <eal.prog@gmail.com>
2015-10-24 12:17:06 +1100

    add ReimportUnityEngineUI and uGUITools in namespace Caveman.CustomTools

--- Assets/Editor/uGUITools.cs
+++ Assets/Editor/uGUITools.cs
@@ -2,36 +2,43 @@
 using UnityEditor;
 using UnityEngine;
 
-public class uGUITools : MonoBehaviour 
+namespace Caveman.CustomTools
 {
-    
-    [MenuItem("Tools/uGui Anchors to Corners %&a")]
-	static void AnchorsToCorners(){
-		foreach(Transform transform in Selection.transforms){
-			RectTransform t = transform as RectTransform;
-			RectTransform pt = Selection.activeTransform.parent as RectTransform;
+	public class uGUITools : MonoBehaviour
+	{
 
-			if(t == null || pt == null) return;
-			
-			Vector2 newAnchorsMin = new Vector2(t.anchorMin.x + t.offsetMin.x / pt.rect.width,
-			                                    t.anchorMin.y + t.offsetMin.y / pt.rect.height);
-			Vector2 newAnchorsMax = new Vector2(t.anchorMax.x + t.offsetMax.x / pt.rect.width,
-			                                    t.anchorMax.y + t.offsetMax.y / pt.rect.height);
+		[MenuItem("Tools/uGui Anchors to Corners %&a")]
+		static void AnchorsToCorners()
+		{
+			foreach (Transform transform in Selection.transforms)
+			{
+				RectTransform t = transform as RectTransform;
+				RectTransform pt = Selection.activeTransform.parent as RectTransform;
 
-			t.anchorMin = newAnchorsMin;
-			t.anchorMax = newAnchorsMax;
-			t.offsetMin = t.offsetMax = new Vector2(0, 0);
+				if (t == null || pt == null) return;
+
+				Vector2 newAnchorsMin = new Vector2(t.anchorMin.x + t.offsetMin.x / pt.rect.width,
+													t.anchorMin.y + t.offsetMin.y / pt.rect.height);
+				Vector2 newAnchorsMax = new Vector2(t.anchorMax.x + t.offsetMax.x / pt.rect.width,
+													t.anchorMax.y + t.offsetMax.y / pt.rect.height);
+
+				t.anchorMin = newAnchorsMin;
+				t.anchorMax = newAnchorsMax;
+				t.offsetMin = t.offsetMax = new Vector2(0, 0);
+			}
 		}
-	}
 
-    [MenuItem("Tools/uGui Corners to Anchors %&c")]
-	static void CornersToAnchors(){
-		foreach(Transform transform in Selection.transforms){
-			RectTransform t = transform as RectTransform;
+		[MenuItem("Tools/uGui Corners to Anchors %&c")]
+		static void CornersToAnchors()
+		{
+			foreach (Transform transform in Selection.transforms)
+			{
+				RectTransform t = transform as RectTransform;
 
-			if(t == null) return;
+				if (t == null) return;
 
-			t.offsetMin = t.offsetMax = new Vector2(0, 0);
+				t.offsetMin = t.offsetMax = new Vector2(0, 0);
+			}
 		}
 	}
 }
