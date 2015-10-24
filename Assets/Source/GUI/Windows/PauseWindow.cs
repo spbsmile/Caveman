28c8d80c1ec7c3d43d79b5aaedf9f0d63956445f
VegK <eal.prog@gmail.com>
2015-10-24 13:09:38 +1100

    disable joystick during pause

--- Assets/Source/GUI/Windows/PauseWindow.cs
+++ Assets/Source/GUI/Windows/PauseWindow.cs
@@ -5,17 +5,20 @@ namespace Caveman.UI.Windows
     public class PauseWindow : MonoBehaviour
     {
         public Transform window;
+        public CNJoystick joystick;
 
         public void ResumeGame()
         {
             Time.timeScale = 1;
             window.gameObject.SetActive(false);
+            joystick.Enable();
         }
 
         public void PauseGame()
         {
             Time.timeScale = 0.000001f;
             window.gameObject.SetActive(true);
+            joystick.Disable();
             //TODO stop all animations
         }
 
