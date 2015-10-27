9743b12fbddabdf2d0deb88644a16d7d9481ea2c
VegK <eal.prog@gmail.com>
2015-10-27 14:58:05 +1100

    Disable joystick in Result Window

--- Assets/Source/GUI/Windows/ResultRound.cs
+++ Assets/Source/GUI/Windows/ResultRound.cs
@@ -5,16 +5,19 @@ namespace Caveman.UI.Windows
 {
     public class ResultRound : Result
     {
+        public CNJoystick joystick;
+
         public void OnEnable()
         {
             if (!Setting.Settings.multiplayerMode)
             {
-                StartCoroutine(DisplayResult());    
+                StartCoroutine(DisplayResult());
             }
         }
 
         protected override IEnumerator DisplayResult()
         {
+            joystick.Disable();
             Time.timeScale = 0.00001f;
             yield return StartCoroutine(base.DisplayResult());
         }
