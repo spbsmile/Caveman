95d54b98483942c792bbfbe0f5a67819e48e16a5
VegK <eal.prog@gmail.com>
2015-12-02 23:28:50 +1100

    Smooth show the start game

--- Assets/Source/GUI/Common/LoadingScreen.cs
+++ Assets/Source/GUI/Common/LoadingScreen.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using Caveman.Utils;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,12 +12,16 @@ namespace Caveman.UI.Common
         public Transform root;
         public Image blackscreen;
         public float TimeEmergenceBlackScreen = 0.2f;
+        public float TimeHidingBlackScreen = 0.5f;
+
+        public event EventHandler FinishLoading;
 
         public override void Awake()
         {
             if (!instance)
             {
                 root.gameObject.SetActive(false);
+                blackscreen.gameObject.SetActive(false);
             }
             else
             {
@@ -44,6 +49,7 @@ namespace Caveman.UI.Common
         private IEnumerator WithProgress(AsyncOperation load)
         {
             root.gameObject.SetActive(true);
+            blackscreen.gameObject.SetActive(true);
 
             while (!load.isDone)
             {
@@ -51,6 +57,7 @@ namespace Caveman.UI.Common
                 yield return new WaitForFixedUpdate();
             }
 
+            // Hide loading screen
             var color = blackscreen.color;
             color.a = 0;
             blackscreen.color = color;
@@ -62,8 +69,21 @@ namespace Caveman.UI.Common
                 blackscreen.color = c;
                 yield return null;
             }
-
             root.gameObject.SetActive(false);
+            
+            // Show game screen
+            startTime = Time.time;
+            while (blackscreen.color.a > 0)
+            {
+                var c = blackscreen.color;
+                c.a = Mathf.Lerp(1, 0, (Time.time - startTime) / TimeHidingBlackScreen);
+                blackscreen.color = c;
+                yield return null;
+            }
+
+            blackscreen.gameObject.SetActive(false);
+            if (FinishLoading != null)
+                FinishLoading(this, EventArgs.Empty);
         }
     }
 }
