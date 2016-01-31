2ba8f5985abe0150f29d75f8091567117a0d93c7
Makar <zabiyakin.makar@gmail.com>
2016-02-01 00:05:56 +0300

    add IClientListener, initial improve code sockets

---
+++ Assets/Source/Network/socket/IClientListener.cs
@@ -0,0 +1,17 @@
+﻿namespace Caveman.Network
+{
+    public interface IClientListener
+    {
+        void LoginMessage();
+        void TickMessage();
+        void PickWeapon();
+        void UseWeapon();
+        void PickBonus();
+        void PlayerGold();
+        void Respawn();
+        void PlayerDead();
+        void AddedKillStat();
+        void Move();
+        void Logout();
+    }
+}
