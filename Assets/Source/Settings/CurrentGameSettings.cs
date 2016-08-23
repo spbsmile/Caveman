4b84858df6a7d5d75a84e72c9451d260ceda5397
Makar Zabiyakin <zabiyakin.makar@gmail.com>
2016-08-24 00:06:42 +0300

    temporary comment code of config level

--- Assets/Source/Settings/CurrentGameSettings.cs
+++ Assets/Source/Settings/CurrentGameSettings.cs
@@ -12,12 +12,12 @@ namespace Caveman.Setting
         public readonly Dictionary<string, WeaponConfig> WeaponsConfigs = new Dictionary<string, WeaponConfig>();
         public readonly Dictionary<string, ImageConfig> ImagesConfigs = new Dictionary<string, ImageConfig>();
         public readonly Dictionary<string, PoolsConfig> PoolsConfigs = new Dictionary<string, PoolsConfig>();
-        public readonly Dictionary<string, LevelSingleConfig> LevelsSingleConfigs = new Dictionary<string, LevelSingleConfig>();
+      /*  public readonly Dictionary<string, LevelSingleConfig> LevelsSingleConfigs = new Dictionary<string, LevelSingleConfig>(); */
         public readonly Dictionary<string, LevelMultiplayerConfig> LevelsMultiplayerConfigs = new Dictionary<string, LevelMultiplayerConfig>();
 
         private CurrentGameSettings(IEnumerable<BonusConfig> bonusesConfigs, IEnumerable<PlayerConfig> playersConfigs, IEnumerable<WeaponConfig> weaponsConfigs,
           IEnumerable<ImageConfig> imagesConfigs,
-          IEnumerable<PoolsConfig> poolsConfig, IEnumerable<LevelSingleConfig> levelsSingleConfigs, IEnumerable<LevelMultiplayerConfig> levelsMultiplayerConfigs)
+          IEnumerable<PoolsConfig> poolsConfig)//, IEnumerable<LevelSingleConfig> levelsSingleConfigs, IEnumerable<LevelMultiplayerConfig> levelsMultiplayerConfigs)
         {
             foreach (var config in bonusesConfigs)
             {
@@ -43,7 +43,7 @@ namespace Caveman.Setting
             {
                 PoolsConfigs.Add(config.Name, config);
             }
-
+            /*
             foreach (var config in levelsSingleConfigs)
             {
                 LevelsSingleConfigs.Add(config.Name, config);
@@ -52,7 +52,7 @@ namespace Caveman.Setting
             foreach (var config in levelsMultiplayerConfigs)
             {
                 LevelsMultiplayerConfigs.Add(config.Name, config);
-            }
+            }*/
         }
 
         public static
@@ -64,10 +64,10 @@ namespace Caveman.Setting
                 SettingsHandler.ParseSettingsFromFile<List<PlayerConfig>>(players),
                 SettingsHandler.ParseSettingsFromFile<List<WeaponConfig>>(weapons),
                 SettingsHandler.ParseSettingsFromFile<List<ImageConfig>>(images),
-                SettingsHandler.ParseSettingsFromFile<List<PoolsConfig>>(pools),
+                SettingsHandler.ParseSettingsFromFile<List<PoolsConfig>>(pools)/*,
                 SettingsHandler.ParseSettingsFromFile<List<LevelSingleConfig>>(levelsSingle),
                 SettingsHandler.ParseSettingsFromFile<List<LevelMultiplayerConfig>>(levelsMultiplayer)
-                );
+                */);
         }
     }
 }
