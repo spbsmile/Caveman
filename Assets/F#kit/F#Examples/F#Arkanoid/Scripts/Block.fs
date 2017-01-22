b4835cf0d05fcc34fda5b500cfdc70c8c14e1b4a
Makar Zabiyakin <zabiyakin.makar@gmail.com>
2017-01-22 21:14:36 +0300

    delete F#

--- Assets/F#kit/F#Examples/F#Arkanoid/Scripts/Block.fs
+++
@@ -1,9 +0,0 @@
-namespace FSharp
-open UnityEngine
- 
-type Block() = 
-    inherit MonoBehaviour()
-
-    // destroy self on collision
-    member this.OnCollisionEnter2D(collisionInfo : Collision2D) =
-        GameObject.Destroy this.gameObject
--- Assets/F#kit/F#Examples/F#HelloWorld/HelloWorld.fs
+++
@@ -1,9 +0,0 @@
-namespace FSharp
-open UnityEngine
- 
-type HelloWorld() = 
-    inherit MonoBehaviour()
-    member this.Start() = Debug.Log("Hello World")
-
-
-    
