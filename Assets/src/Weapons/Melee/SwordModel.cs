f0c96bf0970be21277ba3005ab81290323721620
Makar Zabiyakin <zabiyakin.makar@gmail.com>
2017-04-19 00:14:47 +0300

    animation sword

--- Assets/src/Weapons/Melee/SwordModel.cs
+++ Assets/src/Weapons/Melee/SwordModel.cs
@@ -5,14 +5,22 @@ namespace Caveman.Weapons.Melee
 {
     public class SwordModel : WeaponModelBase, IWeapon
     {
-        public void Activate(string ownerId, Vector2 from, Vector2 to)
-        {
-            StartCoroutine(ContinuousRotation());
-        }
+        private const float IntervalRotate = 20f;
+        private const float StepRotate = 5f;
+        private const int LayerWhenActive = 15;
+        private const int LayerWhenInActive = 0;
+        private SpriteRenderer render;
 
         public void Awake()
         {
+            render = GetComponent<SpriteRenderer>(); 
             Config = EnterPoint.Configs.Weapon["sword"];
+        }
+
+        public void Activate(string ownerId, Vector2 from, Vector2 to)
+        {
+            render.sortingOrder = LayerWhenActive;
+            StartCoroutine(ContinuousRotation());
         }
 
         public override void Take()
@@ -20,13 +28,24 @@ namespace Caveman.Weapons.Melee
             print("hello my sword");
         }
 
-        IEnumerator ContinuousRotation()
+        private IEnumerator ContinuousRotation()
         {
-            while (true)
+            yield return new WaitForSeconds(0.05f);
+            var angle = 0f;
+            while (angle < IntervalRotate)
             {
-                transform.Rotate(Vector3.forward, 10);
+                angle += StepRotate;
+                transform.Rotate(Vector3.forward, angle);
                 yield return new WaitForSeconds(0.01f);
             }
+            yield return new WaitForSeconds(1.5f);
+            Sheathe();
+        }
+
+        private void Sheathe()
+        {
+            render.sortingOrder = LayerWhenInActive;
+            transform.rotation = Quaternion.identity;
         }
 
     }
